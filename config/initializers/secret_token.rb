57606fc5bc91830505a478157a74a8c93f2ed278
David Oberman <danishgorounds-github@yahoo.com>
2012-12-04 14:16:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stamper::Application.config.secret_token = 'b02951ce6eafbb05afee77ecc33350a5e3ecbe2b0502d77c8a301c588b13dd051fb3236093936cf3251b85d0fc2acce6e04be23e17ca30e722f616a206706aff'
